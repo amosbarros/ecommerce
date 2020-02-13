5a6b60a4536b5c3f7fe138560a30b85f0bd1ee4c
amosbarros <cacomos.barros@gmail.com>
2020-02-13 20:25:51 -0300

    removendo arquivos de configuração

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,8 +6,18 @@ namespace Composer\Autoload;
 
 class ComposerStaticInitfedc4490ff4b62127ae72cd04cf93c33
 {
-    public static $fallbackDirsPsr4 = array (
-        0 => __DIR__ . '/..' . '/hcodebr/php-classes/src',
+    public static $prefixLengthsPsr4 = array (
+        'H' => 
+        array (
+            'Hcode\\' => 6,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'Hcode\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/hcodebr/php-classes/src',
+        ),
     );
 
     public static $prefixesPsr0 = array (
@@ -41,7 +51,8 @@ class ComposerStaticInitfedc4490ff4b62127ae72cd04cf93c33
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->fallbackDirsPsr4 = ComposerStaticInitfedc4490ff4b62127ae72cd04cf93c33::$fallbackDirsPsr4;
+            $loader->prefixLengthsPsr4 = ComposerStaticInitfedc4490ff4b62127ae72cd04cf93c33::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInitfedc4490ff4b62127ae72cd04cf93c33::$prefixDirsPsr4;
             $loader->prefixesPsr0 = ComposerStaticInitfedc4490ff4b62127ae72cd04cf93c33::$prefixesPsr0;
             $loader->classMap = ComposerStaticInitfedc4490ff4b62127ae72cd04cf93c33::$classMap;
 
---
+++ viwes-cache/header.8e6c3c8ceeae396d91d66ff58af5815d.rtpl.php
@@ -0,0 +1,8 @@
+<?php if(!class_exists('Rain\Tpl')){exit;}?><!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="utf-8">
+    <title>hcodecommerce</title>
+</head>
+<body>
+    
