565e61c4d877b2601822363f77419e55ed9807e5
amosbarros <cacomos.barros@gmail.com>
2020-02-14 17:49:38 -0300

    não esta direcionando para o admim

--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -9,25 +9,28 @@ class Page{
     private $tpl;
     private $options = [];
     private $defaults = [
+        "header"=>true,
+        "footer"=>true,
         "data" =>[] 
     ];
 
-    public function __construct($opts = array()){
+    public function __construct($opts = array(), $tpl_dir = "/views/"){
         
         $this->options = array_merge($this->defaults, $opts);
         
         $config = array(
-            "tpl_dir"       => $_SERVER["DOCUMENT_ROOT"]."/views/",
-            "cache_dir"     => $_SERVER["DOCUMENT_ROOT"]."/viwes-cache/",
+            "tpl_dir"       => $_SERVER["DOCUMENT_ROOT"].$tpl_dir,
+            "cache_dir"     => $_SERVER["DOCUMENT_ROOT"]."/views-cache/",
             "debug"         => false
 		);
 
         Tpl::configure( $config );
         
         $this->tpl = new Tpl;
+
         $this->setData($this->options["data"]);
 
-        $this->tpl->draw("header");
+        if($this->options["header"]===true) $this->tpl->draw("header");
     }
 
     private function setData($data = array()){
@@ -39,16 +42,15 @@ class Page{
     public function setTpl($name, $data = array(), $returnHTML = false){
         
             $this->setData($data);
+
             return $this->tpl->draw($name, $returnHTML);
-        
-
 
     }
 
-    public function _destruct(){
-        $this->tpl->draw("footer");
+    public function __destruct(){
+        if($this->options["footer"]===true) $this->tpl->draw("footer");
 
-    }
+    } 
     
 }
 
--- viwes-cache/header.8e6c3c8ceeae396d91d66ff58af5815d.rtpl.php
+++
@@ -1,8 +0,0 @@
-<?php if(!class_exists('Rain\Tpl')){exit;}?><!DOCTYPE html>
-<html lang="pt-BR">
-<head>
-    <meta charset="utf-8">
-    <title>hcodecommerce</title>
-</head>
-<body>
-    
